5a31fd921d195b4e1df47d22a6bf3779be5c2644
tori ten <kawano@bijicom.jp>
2025-07-06 21:49:52 +0900

    second tasklist

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar bg-neutral text-neutral-content">
         {{-- トップページへのリンク --}}
         <div class="flex-1">
-            <h1><a class="btn btn-ghost normal-case text-xl" href="/">TASKBoard</a></h1>
+            <h1><a class="btn btn-ghost normal-case text-xl" href="/">タスク管理</a></h1>
         </div>
 
         <div class="flex-none">
