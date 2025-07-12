e7a644ff3a7114dfd01ad5a1ebf459912aa57715
tori ten <kawano@bijicom.jp>
2025-07-13 04:32:13 +0900

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -36,7 +36,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -75,8 +81,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -11,8 +11,14 @@
             @csrf
 
                 <div class="form-control my-4">
+                    <label for="status" class="label">
+                        <span class="label-text">ステータス:</span>
+                    </label>
+                    <input type="text" name="status" class="input input-bordered w-full">
+                </div>
+                <div class="form-control my-4">
                     <label for="content" class="label">
-                        <span class="label-text">メッセージ:</span>
+                        <span class="label-text">タスク:</span>
                     </label>
                     <input type="text" name="content" class="input input-bordered w-full">
                 </div>
